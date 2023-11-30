247921e720bf868007721799dca83b7f1c1295e7
Felipe Dobrinin <felipedobrinin@gmail.com>
2023-11-30 13:52:05 -0300

    mucha porqueria

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import Panel from './components/Panel';
 
 function App() {
@@ -8,14 +8,52 @@ function App() {
   const [numbers,setNumbers] = useState(true);
   const [special,setSpecial] = useState(false);
 
-  const [lenght, setLenght] = useState(8);
+  const [length, setLenght] = useState(8);
 
-  function GeneratePassword(e) {
-    e.preventDefault();
-    let password = "locura";
-    console.log("New Password ", password)
-    console.log({"caps":caps, "lowercase":lowercase, "numbers":numbers, "special":special})
+  const [password,setPassword] = useState("");
+
+  function copy_to_clipboard(){
+       navigator.clipboard.writeText(password);
+       alert("Copiado");
   }
+
+    function handleGenerate(e){
+        e.preventDefault();
+        GeneratePassword();
+    }
+  
+  function GeneratePassword() {
+    let pool = [];
+    if (lowercase){
+        let lowercase_array=  Array.from({ length: 26 }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i)); 
+      pool.push(...lowercase_array);
+    }
+    if (caps){
+        let caps_array = Array.from({ length: 26 }, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
+      pool.push(...caps_array);
+    }
+    if (numbers){
+        let numbers_array = Array.from({ length: 10 }, (_, i) => String.fromCharCode('0'.charCodeAt(0) + i));
+      pool.push(...numbers_array);
+    }
+    if (special){
+        pool.push(...['!','#','$','%','*','?','@','^','&']);
+    }
+    console.log({"caps":caps, "lowercase":lowercase, "numbers":numbers, "special":special})
+    console.log(pool);
+    let new_password = "";
+    for ( let i = 0; i < length; i++ ) {
+        let char = pool[(Math.floor(Math.random() * pool.length))];
+        console.log(char);
+        new_password += char ;
+    }
+    console.log("after for");
+    setPassword(new_password);
+  }
+  
+  useEffect(() => { 
+        GeneratePassword();
+    },[]);
 
   return (
     <div className="app p-2">
@@ -24,11 +62,11 @@ function App() {
         <p className=""></p> 
 
         <div className='password text-lg'>
-          <span className='p-2'>Ai*j2_?WI;</span>
-          <div className='copy-button border-l-2 p-2 hover:cursor-pointer'>📋</div>
+          <span className='p-2'>{password}</span>
+          <div className='copy-button border-l-2 p-2 hover:cursor-pointer' onClick={copy_to_clipboard}>📋</div>
         </div>
 
-        <form action="" className='py-2' onSubmit={GeneratePassword} >
+        <form action="" className='py-2' onSubmit={handleGenerate} >
           <div className='checkform'>
             <input type="checkbox" name="" id="caps" className='checkbox' 
               checked={caps}
@@ -59,11 +97,32 @@ function App() {
           </div>
 
           <div className='rangeform'>
-            <input type="range" name="" id="range" className='range'
+            <div className='flex gap-4'>
+              <label htmlFor="range">Lenght</label>
+              <input type="number" className='w-14'
+                min="8"
+                max="256"
+                value={length} 
+                onChange={(e) => setLenght(e.target.value)}
+              />
+            </div>
+            
+            <input type="range" className='range'
               min="8"
               max="256"
+              list="values"
+              value={length}
+              onChange={(e) => setLenght(e.target.value)}
             />
-            <label htmlFor="range">Special Characters</label>
+
+            <datalist id="values">
+              <option value="8" label="8"></option>
+              <option value="16" label="16"></option>
+              <option value="32" label="32"></option>
+              <option value="64" label="64"></option>
+              <option value="128" label="128"></option>
+              <option value="256" label="256"></option>
+            </datalist>
           </div>
 
           <input type="submit" value="Re Generate" className='mt-4 mx-auto text-2xl button p-2 px-10 '/>
