ed433d337d047122d51c65b52668be47b2ea3359
Felipe Dobrinin Salavesqui <felipedobrinin@gmail.com>
2023-12-05 15:58:51 -0300

    added : alert when unsuficient options

--- src/App.jsx
+++ src/App.jsx
@@ -28,34 +28,27 @@ function App() {
     let pool = [];
     let _length = length;
     let special_length = 0;
-    if (lowercase) {
-      let lowercase_array = Array.from({ length: 26 }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i));
-      pool.push(...lowercase_array);
+    if ((lowercase || caps || numbers ) == false ) {
+      alert("You need at least one option (apart from special characters) enabled");
+      return;
     }
-    if (caps) {
-      let caps_array = Array.from({ length: 26 }, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i));
-      pool.push(...caps_array);
-    }
-    if (numbers) {
-      let numbers_array = Array.from({ length: 10 }, (_, i) => String.fromCharCode('0'.charCodeAt(0) + i));
-      pool.push(...numbers_array);
-    }
+    if (lowercase) pool.push(...Array.from({ length: 26 }, (_, i) => String.fromCharCode('a'.charCodeAt(0) + i)));
+    if (caps) pool.push(...Array.from({ length: 26 }, (_, i) => String.fromCharCode('A'.charCodeAt(0) + i)));
+    if (numbers) pool.push(...Array.from({ length: 10 }, (_, i) => String.fromCharCode('0'.charCodeAt(0) + i)));
 
+    // here we are generating a number of special characters to use
+    // making sure that we get at least 1/4 of the total characters made of special characters
     if (special) {
-      // here we are generating a number of special characters to use
-      // making sure that we get at least 1/4 of the total characters made of special characters
       let factor = Math.floor(Math.random() * 3) + 2;
       special_length = Math.floor(length / factor);
-
       _length -= special_length;
     }
-    let new_password = "";
 
+    let new_password = "";
     for (let i = 0; i < _length; i++) {
       let char = pool[(Math.floor(Math.random() * pool.length))];
       new_password += char;
     }
-
     //adding special characters in different loop, we are making sure that we get a decent ammount of special characters here
     for (let i = 0; i < special_length; i++) {
       let char = special_chars[(Math.floor(Math.random() * special_chars.length))];
@@ -80,6 +73,7 @@ function App() {
   function replaceAt(string, index, replacement) {
     return string.substring(0, index) + replacement + string.substring(index + replacement.length);
   }
+
   useEffect(() => {
     GeneratePassword();
   }, []);
