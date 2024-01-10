af4df8d6e589fbef2c0ca554540ad24f0ba39688
Felipe Dobrinin Salavesqui <felipedobrinin@gmail.com>
2024-01-10 15:17:22 -0300

    better styling

--- src/components/Password.jsx
+++ src/components/Password.jsx
@@ -17,7 +17,7 @@ function Password({ password, special_chars }) {
     }
     return (
         <div className="password">
-            <div className="password-text p-2 text-4xl md:text-3xl font-medium">
+            <div className="font-mono font-semibold password-text p-2 text-5xl ">
                 {password.split("").map((char) => renderChar(char))}
             </div>
             <div className="copy-button border-l-2 p-2 hover:cursor-pointer text-4xl md:text-2xl" onClick={copy_to_clipboard}>📋</div>
