aa7bb5912effb3ea8ea84ad330c30a12e35c0455
Felipe Dobrinin Salavesqui <felipedobrinin@gmail.com>
2024-01-11 16:12:31 -0300

    changes to the way options lenghts are made

--- src/App.jsx
+++ src/App.jsx
@@ -8,19 +8,81 @@ function App() {
   // TODO Make it posible to choose them 
   var special_chars = ['!', '#', '$', '%', '*', '?', '@', '^', '&'];
   // Checkboxes por password options
-  const [caps, setCaps] = useState(true);
-  const [lowercase, setLowercase] = useState(true);
-  const [numbers, setNumbers] = useState(true);
-  const [special, setSpecial] = useState(true);
-  // passowrd length
-  const [length, setLenght] = useState(16);
+  const [params,setParams] = useState({
+    length : 16,
+    has_lowercase : true,
+    lowercase_len : 4,
+    has_uppercase : true,
+    uppercase_len : 4,
+    has_numbers : true,
+    numbers_len : 4,
+    has_special_chars : true,
+    special_chars_len : 4
+  });
 
-  const { password, generatePassword } = usePassword(length, lowercase, caps, numbers, special, special_chars);
+  const { password, generatePassword } = usePassword(params, special_chars);
 
   function handleGenerate(e) {
     e.preventDefault();
+
+    if ((params.has_lowercase || params.has_uppercase || params.has_numbers) == false) {
+        alert("You need at least one option (apart from special characters) enabled");
+        return;
+    }
+
+    if (!params.has_lowercase && !params.has_uppercase) {
+        alert("You need at least one type of letter (a-z or A-Z) available");
+        return;
+    }
+
+    setParams({
+      ...params,
+      ...lengths()
+    })
     generatePassword();
   }
+
+  function lengths() {
+    let left = params.length;
+    let factor = [params.has_lowercase, params.has_uppercase, params.has_numbers, params.has_special_chars].filter(Boolean).length;
+
+    let lowercase_len = 0;
+    let uppercase_len = 0;
+    let numbers_len = 0;
+    let special_chars_len = 0;
+
+
+    if (params.has_lowercase) { 
+      lowercase_len = Math.floor(left / factor); 
+      left -= lowercase_len;
+      console.log(left);
+    }
+    if (params.has_uppercase) { 
+      uppercase_len = Math.floor(left / factor); 
+      left -= uppercase_len;
+      console.log(left);
+    }
+    if (params.has_numbers) { 
+      numbers_len = Math.floor(left / factor); 
+      left -= numbers_len;
+      console.log(left);
+    }
+    if (params.has_special_chars) { 
+      special_chars_len = Math.floor(left / factor); 
+      left -= special_chars_len;
+      console.log(left);
+    }
+    let lengths = {
+      lowercase_len,
+      uppercase_len,
+      numbers_len,
+      special_chars_len
+    }
+
+    console.log(lengths)
+    return lengths
+  }
+
 
   useEffect(() => {
     generatePassword();
@@ -34,41 +96,56 @@ function App() {
         <form action="" className="py-2" onSubmit={handleGenerate} >
           <div className="checkform">
             <input type="checkbox" name="" id="caps" className="checkbox"
-              checked={caps}
-              onChange={() => setCaps(!caps)}
+              checked={params.has_uppercase}
+              onChange={() => setParams({
+                ...params,
+                has_uppercase : !params.has_uppercase
+              })}
             />
-            <label htmlFor="caps">A-Z</label>
+            <label htmlFor="caps">Uppercase (A-Z)</label>
           </div>
           <div className="checkform">
             <input type="checkbox" name="" id="lowercase" className="checkbox"
-              checked={lowercase}
-              onChange={() => setLowercase(!lowercase)}
+              checked={params.has_lowercase}
+              onChange={() => setParams({
+                ...params,
+                has_lowercase : !params.has_lowercase
+              })}
             />
-            <label htmlFor="lowercase">a-z</label>
+            <label htmlFor="lowercase">Lowercase (a-z)</label>
           </div>
           <div className="checkform">
             <input type="checkbox" name="" id="numbers" className="checkbox"
-              checked={numbers}
-              onChange={() => setNumbers(!numbers)}
+              checked={params.has_numbers}
+              onChange={() => setParams({
+                ...params,
+                has_numbers : !params.has_numbers
+              })}
             />
-            <label htmlFor="numbers">0-9</label>
+            <label htmlFor="numbers">Numbers (0-9)</label>
           </div>
           <div className="checkform">
             <input type="checkbox" name="" id="special" className="checkbox"
-              checked={special}
-              onChange={() => setSpecial(!special)}
+              checked={params.has_special_chars}
+              onChange={() => setParams({
+                ...params,
+                has_special_chars : !params.has_special_chars
+              })}
             />
             <label htmlFor="special">Special Characters</label>
           </div>
 
           <div className="rangeform">
             <div className="flex gap-4">
-              <label htmlFor="range">Lenght</label>
+              <label htmlFor="range">Length</label>
               <input type="number" className="w-16 md:w-14"
                 min="8"
                 max="256"
-                value={length}
-                onChange={(e) => setLenght(e.target.value)}
+                value={params.length}
+                onChange={(e) => setParams({
+                  ...params,
+                  length : e.target.value
+                })}
               />
             </div>
 
@@ -76,8 +153,11 @@ function App() {
               min="8"
               max="256"
               list="values"
-              value={length}
-              onChange={(e) => setLenght(e.target.value)}
+              value={params.length}
+              onChange={(e) => setParams({
+                ...params,
+                length : e.target.value
+              })}
             />
 
             <datalist id="values">
