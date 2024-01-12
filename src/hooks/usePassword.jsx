48c732d52b45107f02823a3239741e95ba1574b5
Felipe Dobrinin Salavesqui <felipedobrinin@gmail.com>
2024-01-12 15:45:03 -0300

    removed emtpy space

--- src/hooks/usePassword.jsx
+++ src/hooks/usePassword.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 
 function usePassword(special_chars) {
-
     const [password, setPassword] = useState("");
 
     // Generates a random password with the specified parameters
@@ -65,4 +64,5 @@ function random_numbers(len){
     }
     return string;
 }
+
 export default usePassword
